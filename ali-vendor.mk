65904b6d9cf863b6ba55a7f7673589f5a0f23d34
RealJohnGalt <johngalt@fake.mail>
2019-06-10 10:40:35 -0700

    building wlan

--- ali-vendor.mk
+++ ali-vendor.mk
@@ -406,7 +406,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/ali/proprietary/etc/permissions/com.motorola.omadm.sprint.xml:system/etc/permissions/com.motorola.omadm.sprint.xml \
     vendor/motorola/ali/proprietary/etc/permissions/com.motorola.omadm.vzw.xml:system/etc/permissions/com.motorola.omadm.vzw.xml \
     vendor/motorola/ali/proprietary/etc/permissions/org.codeaurora.ims.xml:system/etc/permissions/org.codeaurora.ims.xml \
-    vendor/motorola/ali/proprietary/vendor/etc/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_cfg.dat \
     vendor/motorola/ali/proprietary/vendor/etc/wifi/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_wlan_nv.bin \
     vendor/motorola/ali/proprietary/vendor/etc/wifi/WCNSS_qcom_wlan_nv_Argentina.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_wlan_nv_Argentina.bin \
     vendor/motorola/ali/proprietary/vendor/etc/wifi/WCNSS_qcom_wlan_nv_Brazil.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_wlan_nv_Brazil.bin \
@@ -633,8 +632,9 @@ PRODUCT_COPY_FILES += \
 
 
 # wlan
-PRODUCT_COPY_FILES += \
-    vendor/motorola/ali/proprietary/vendor/lib/modules/wlan.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/wlan.ko
+#PRODUCT_COPY_FILES += \
+#    vendor/motorola/ali/proprietary/vendor/lib/modules/wlan.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/wlan.ko \
+#    vendor/motorola/ali/proprietary/vendor/lib/modules/wlan.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/pronto/pronto_wlan.ko
 
 #Keymaster
 PRODUCT_COPY_FILES += \
